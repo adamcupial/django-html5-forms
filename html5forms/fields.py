2e617d7dbcdacf1a807a16d233366b7255173cf9
Trevor Hunsaker <saturdayplace@gmail.com>
2012-01-20 13:25:06 -0700

    Added Html5ChoiceField class to fields.py

--- html5forms/fields.py
+++ html5forms/fields.py
@@ -28,12 +28,14 @@ class Html5Field(forms.fields.Field):
     :type autofocus: Boolean
     """
 
-    def __init__(self, placeholder=None, autofocus=False, class_attr=[],
+    def __init__(self, choices=(), placeholder=None, autofocus=False, class_attr=[],
             *args, **kwargs):
         self.placeholder = placeholder
         self.autofocus = autofocus
         self.class_attr = class_attr
+        self.choices = choices
         super(Html5Field, self).__init__(*args, **kwargs)
+        
 
     def widget_attrs(self, widget):
         widget_attrs = super(Html5Field, self).widget_attrs(widget)
@@ -50,7 +52,7 @@ class Html5Field(forms.fields.Field):
             current_class.append('required')
 
         if isinstance(self.class_attr, (str, unicode)):
-            self.class_attr = self.class_attr.split()
+            self.class_attr = self.class_attr.split()    
 
         for classitem in self.class_attr:
             if classitem not in current_class:
@@ -311,3 +313,54 @@ class Html5RangeField(Html5IntegerField):
         if self.step is not None:
             par_attrs.update({'step': str(self.step)})
         return par_attrs
+
+
+class Html5ChoiceField(Html5Field):
+    widget = forms.Select
+    default_error_messages = {
+        'invalid_choice': _(u'Select a valid choice. %(value)s is not one of the available choices.'),
+    }
+
+    def __init__(self, choices=(), required=True, widget=None, label=None,
+                 initial=None, help_text=None, *args, **kwargs):
+        super(Html5ChoiceField, self).__init__(required=required, widget=widget, label=label,
+                                        initial=initial, help_text=help_text, *args, **kwargs)
+        self.choices = choices
+
+    def _get_choices(self):
+        return self._choices
+
+    def _set_choices(self, value):
+        # Setting choices also sets the choices on the widget.
+        # choices can be any iterable, but we call list() on it because
+        # it will be consumed more than once.
+        self._choices = self.widget.choices = list(value)
+
+    choices = property(_get_choices, _set_choices)
+
+    def to_python(self, value):
+        "Returns a Unicode object."
+        if value in validators.EMPTY_VALUES:
+            return u''
+        return smart_unicode(value)
+
+    def validate(self, value):
+        """
+        Validates that the input is in self.choices.
+        """
+        super(ChoiceField, self).validate(value)
+        if value and not self.valid_value(value):
+            raise ValidationError(self.error_messages['invalid_choice'] % {'value': value})
+
+    def valid_value(self, value):
+        "Check to see if the provided value is a valid choice"
+        for k, v in self.choices:
+            if isinstance(v, (list, tuple)):
+                # This is an optgroup, so look inside the group for options
+                for k2, v2 in v:
+                    if value == smart_unicode(k2):
+                        return True
+            else:
+                if value == smart_unicode(k):
+                    return True
+        return False
